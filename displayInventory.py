932ccdce94042a615ca17f0526be33d9b38d40e6
Jacob Wolken <Jacob.Wolken@vu.com>
2023-04-23 13:10:03 -0500

    :)

--- displayInventory.py
+++ displayInventory.py
@@ -2,7 +2,16 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
+    total_inventory_items = 0
 
+    print("Inventory:")
+    
+    for item, quantity in inventory.items():
+        print(quantity, item)
+        total_inventory_items += quantity
+
+    print(f"Total number of items: {total_inventory_items}")
+    
 
 if __name__ == "__main__":
     displayInventory(stuff)
